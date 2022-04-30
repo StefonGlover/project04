4627de7af51b422585b4b7662606c252386c968b
Stefon <52985099+StefonGlover@users.noreply.github.com>
2022-04-30 10:11:15 -0400

    Created header and footer

--- register.php
+++ register.php
@@ -1,21 +1,25 @@
 <?php include("header.php"); ?>
 <div class="wrapper">
-    <h2>Login</h2>
-    <p>Please fill in your credentials to login.</p>
+    <h2>Register</h2>
+    <p>Please fill in the information below to register.</p>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <div class="form-group">
             <label>Username</label>
-            <input type="text" name="username">
+            <input type="text" name="username" required>
         </div>
         <div class="form-group">
             <label>Password</label>
             <input type="password" name="password">
         </div>
         <div class="form-group">
+            <label>Confirm Password</label>
+            <input type="password" name="password">
+        </div>
+        <div class="form-group">
             <input type="submit" class="btn btn-primary" value="Login">
         </div>
-        <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
     </form>
 </div>
+
 
 <?php include("footer.php"); ?>
